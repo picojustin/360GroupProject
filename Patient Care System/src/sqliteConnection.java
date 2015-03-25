6dd8c3d19c229e8f879b19a08daa7c85e42c49c2
picojustin <picojustin@gmail.com>
2015-03-25 10:57:58 -0700

    Database setup for 360 project

---
+++ Patient Care System/src/sqliteConnection.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+import javax.swing.*;
+
+public class sqliteConnection {
+	Connection conn = null;
+	public static Connection dbConnector(){
+		try{
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite::resource:PatientData.sqlite");
+			JOptionPane.showMessageDialog(null, "Success!");
+			return conn;
+		}
+		
+		catch (Exception e){
+			JOptionPane.showMessageDialog(null, e);
+			return null;
+		}
+	}
+}
